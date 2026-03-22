7064a2d29142da82230a8651f86d9611a170ef0d
morganfisher19 <morganfisher319@gmail.com>
2026-03-22 18:48:09 -0400

    Creating home page design

--- code/frontend/src/components/Home.tsx
+++ code/frontend/src/components/Home.tsx
@@ -3,13 +3,30 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <main>
-      <section>
-        <h1>Congress Search</h1>
-        <h2>119th Congress</h2>
-        <p>Select a member to explore their voting record.</p>
+   <>
+   <main>
+      <section className="hero">
+        {/* Left: stacked/layered images */}
+        <div className="imageStack">
+          <img className="branch" src="../../public/images/branch-2.png" alt="" />
+          <img className="capitol" src="../../public/images/capitol-4.png" alt="" />
+        </div>
+
+        {/* Right: text + member select */}
+        <div className="content">
+          <h1>Congress Search</h1>
+          <h2>119th Congress</h2>
+          <p>Select a member to explore their voting record.</p>
+          <MemberSelect />
+        </div>
       </section>
-      <MemberSelect />
+
+      {/* Repeating banner image */}
+      <div>
+        <div className="repeatBanner" />
+      </div>
+      
     </main>
+    </>
   );
 }
